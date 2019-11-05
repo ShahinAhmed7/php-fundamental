b8f105e80ee28826e7d507450610bd0fda33f367
Shahin <meshahin007@gmail.com>
2019-11-05 22:38:51 +0600

    commit

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php 
 
-$name = "Shahin";
-$lName = "Ahmed";
+// $name = "Shahin";
+// $lName = "Ahmed";
 
-printf("Hi My first name is %s my last name is %s ", $name, $lName);
+// printf("Hi My first name is %s my last name is %s ", $name, $lName);
 
 
-$add = "bangladesh";
-$city = "dhaka";
+// $add = "bangladesh";
+// $city = "dhaka";
 
-printf("\nMy country is %s and city is %s", $add, $city);
+// printf("\nMy country is %s and city is %s", $add, $city);
+
+
+$number = 12;
+
+echo $number+13;
