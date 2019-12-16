613f035833a587b24fbf3c657b1b6375c1494a6f
Shahin <meshahin007@gmail.com>
2019-12-16 07:27:48 +0600

    commit

--- index.php
+++ index.php
@@ -12,6 +12,24 @@
 // printf("\nMy country is %s and city is %s", $add, $city);
 
 
-$number = 12;
+// $number = 12;
 
-echo $number+13;
+// echo $number+13;
+
+// $taka =  100;
+// $taka2 = 100;
+
+// if($taka != $taka2){
+//     echo "$taka is not equal $taka2";
+// } else{
+//     echo "$taka is equal $taka2";
+// }
+
+
+$age = 17;
+
+if($age >= 13 && $age <= 17){
+    echo "This person is a teneger";
+} else{
+    echo "This person is not a teneger";
+}
